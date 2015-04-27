3ea486a2e7ed0b918b25a9da235a4f5f3d74da43
Mike Bignell <mike@mbignell.com>
2015-04-27 10:34:31 +0100

    Made ebay feed info update when adding new items

--- eBay display feed item info.user.js
+++ eBay display feed item info.user.js
@@ -1,19 +1,38 @@
 // ==UserScript==
 // @name         eBay display feed item info
 // @namespace    http://mbignell.com/
-// @version      0.1
+// @version      0.2
 // @description  Shows hover information on feed
 // @author       Mike Bignell
 // @match        http://www.ebay.co.uk/
 // @grant        none
 // ==/UserScript==
 
-var allHoverInfos = document.getElementsByClassName("hover-info");
-
-for(var i = 0; i < allHoverInfos.length; i++)
+function setDivOpacity ()
 {
-    var div = allHoverInfos[i];
-    console.log(div);
-    
-    div.setAttribute("style","opacity: 0.5;");
+    var allHoverInfos = document.getElementsByClassName("hover-info");
+
+    for(var i = 0; i < allHoverInfos.length; i++)
+    {
+        var div = allHoverInfos[i];
+        //console.log(div);
+        
+        if (div.getAttribute("style") == null)
+        {
+            div.setAttribute("style","opacity: 0.5;");
+        }
+    }
 }
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    //console.log(mutations, observer);
+    setDivOpacity();
+});
+
+observer.observe(document, {
+    subtree: true,
+    childList: true,
+});
